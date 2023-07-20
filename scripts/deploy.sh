1a433fac6af0159ace05ef777a2e766d6cb9db41
Saydum Khalibekov <saydum.it@gmail.com>
2023-07-20 12:51:55 +0300

    Minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,8 @@
 
 cd ../ && mvn clean package
 
-echo 'Copy files...'
-
 scp target/saydumru-0.0.1-SNAPSHOT.jar saydum:/root/app/
 
-echo 'Restart server...'
-
 ssh saydum << EOF
-
-pgrep java | xargs kill -9
-java -jar /root/app/saydumru-0.0.1-SNAPSHOT.jar
-
+systemctl restart saydum
 EOF
-
-echo 'Bye'
